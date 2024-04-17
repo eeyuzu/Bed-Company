8b108be4cb44f2f0814a4c43e1fe1cd167912110
eeyuzu <mini5071@naver.com>
2024-04-17 13:59:44 +0900

    침대회사

---
+++ script/index.js
@@ -0,0 +1,16 @@
+// $('header').load('include/header_js.html', function(){
+//     $('.hambuger').click(function(){
+//         $('.hambuger').toggleClass('on')
+//         $('nav').toggleClass('on')
+//     })
+
+//     console.log('k')
+// })
+    
+    
+
+
+$('.hambuger').click(function(){
+    $('.hambuger').toggleClass('on')
+    $('nav').toggleClass('on')
+})
